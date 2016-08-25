3b40b474c7eb4cd3a6b5d18ca07840cbbb8506f4
Vincent Breitmoser <look@my.amazin.horse>
2016-08-25 16:49:11 +0200

    use stylable attributes for openpgp_ colors

---
+++ k9mail/src/main/java/com/fsck/k9/view/ThemeUtils.java
@@ -0,0 +1,25 @@
+package com.fsck.k9.view;
+
+
+import android.content.Context;
+import android.content.res.Resources.Theme;
+import android.support.annotation.AttrRes;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.util.TypedValue;
+
+
+public class ThemeUtils {
+    private static final TypedValue TYPED_VALUE = new TypedValue();
+
+    @ColorInt
+    public static int getStyledColor(Context context, @AttrRes int attr) {
+        return getStyledColor(context.getTheme(), attr);
+    }
+
+    @ColorInt
+    public static synchronized int getStyledColor(Theme theme, @AttrRes int attr) {
+        theme.resolveAttribute(attr, TYPED_VALUE, true);
+        return TYPED_VALUE.data;
+    }
+}
